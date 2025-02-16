363b311330ac47d9425ce0f33d869541d966735d
Nidhin K R <www.nidhinqzee7@gmail.com>
2025-02-16 23:12:11 +0530

    validator set

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,19 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/screens/mainscreen.dart';
 
-class Homescreen extends StatelessWidget {
+class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  TextEditingController _controller = TextEditingController();
+
+  bool _validate = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {
+          setState(() {
+            _validate = false;
+            if (_controller.text.isNotEmpty) {
+              Navigator.push(
+                  context, MaterialPageRoute(builder: (ctx) => MainScreen()));
+            } else {
+              _validate = true;
+            }
+          });
+        },
+        child: Icon(Icons.arrow_forward),
+      ),
       backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          Image.asset(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              "assets/boyIcon.png"),
-        ],
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(top: 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/boyIcon.png"),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "What is your name........?",
+              style: TextStyle(fontSize: 28, fontStyle: FontStyle.italic),
+            ),
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: TextField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  errorText: _validate ? 'Please enter your name' : null,
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
