4e389f55a6b3d40ed3d8f76c4bf3382eda8d1fd5
Nidhin K R <www.nidhinqzee7@gmail.com>
2025-02-17 00:37:23 +0530

    crud operation widgets

--- lib/screens/todo_interface.dart
+++ lib/screens/todo_interface.dart
@@ -13,8 +13,20 @@ class _TodoInterfaceState extends State<TodoInterface> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        title: Text("Enter your ToDo's"),
+        centerTitle: true,
+        backgroundColor: Colors.white,
         automaticallyImplyLeading: true,
       ),
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+          ),
+          itemBuilder: (context, index) {
+            return Card();
+          }),
     );
   }
 }
