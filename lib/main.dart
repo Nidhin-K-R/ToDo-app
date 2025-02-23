e17db84f9e186fe17fec1453248ff690c251b964
Nidhin K R <www.nidhinqzee7@gmail.com>
2025-02-23 19:48:39 +0530

    implement statemanagement

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+
+import 'package:to_do_app/practice/providerdemo.dart';
 import 'package:to_do_app/screens/homescreen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Homescreen(),
-  ));
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ProviderDemo(),
+      child: Consumer<ProviderDemo>(
+        builder: (context, value, child) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: value.mode ? ThemeData.dark() : ThemeData.light(),
+          home: Homescreen(),
+        ),
+      ),
+    );
+  }
 }
